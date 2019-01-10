6032f57dbb46c544489613a6ac52ab9c282d659f
Ohanna Perre <ohanna.perre@gmail.com>
2019-01-10 13:26:56 +0000

    Create an enum of download error

---
+++ IntercomChallengeFramework/IntercomChallengeFramework/Core/FileDownloader/DownloadError.swift
@@ -0,0 +1,14 @@
+//
+//  DownloadError.swift
+//  IntercomChallengeFramework
+//
+//  Created by Ohanna Perre on 10/01/2019.
+//  Copyright © 2019 Ohanna Perre. All rights reserved.
+//
+
+import Foundation
+
+enum DownloadError: Error {
+    case invalidURL
+    case unableToDownload
+}
