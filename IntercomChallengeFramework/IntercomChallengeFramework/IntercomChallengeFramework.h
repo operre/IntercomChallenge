e816045765c4d0334797465aa170d3c4cb79a607
Ohanna Perre <ohanna.perre@gmail.com>
2019-01-10 12:24:53 +0000

    Initial Commit

---
+++ IntercomChallengeFramework/IntercomChallengeFramework/IntercomChallengeFramework.h
@@ -0,0 +1,19 @@
+//
+//  IntercomChallengeFramework.h
+//  IntercomChallengeFramework
+//
+//  Created by Ohanna Perre on 10/01/2019.
+//  Copyright © 2019 Ohanna Perre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IntercomChallengeFramework.
+FOUNDATION_EXPORT double IntercomChallengeFrameworkVersionNumber;
+
+//! Project version string for IntercomChallengeFramework.
+FOUNDATION_EXPORT const unsigned char IntercomChallengeFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IntercomChallengeFramework/PublicHeader.h>
+
+
