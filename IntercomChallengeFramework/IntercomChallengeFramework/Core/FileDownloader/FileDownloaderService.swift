cd19361dd90a1a8d12041bad598c7440792d495d
Ohanna Perre <ohanna.perre@gmail.com>
2019-01-10 13:56:50 +0000

    Create file download service

---
+++ IntercomChallengeFramework/IntercomChallengeFramework/Core/FileDownloader/FileDownloaderService.swift
@@ -0,0 +1,63 @@
+//
+//  FileDownloaderService.swift
+//  IntercomChallengeFramework
+//
+//  Created by Ohanna Perre on 10/01/2019.
+//  Copyright © 2019 Ohanna Perre. All rights reserved.
+//
+
+import Foundation
+
+protocol FileDownloaderServiceProtocol {
+    func download<T: Decodable>(from fileURL: String, responseType: T.Type, handler: @escaping (Result<[T]>) -> Void)
+}
+
+struct FileDownloaderService: FileDownloaderServiceProtocol {
+    private let reader: FileReaderServiceProtocol
+    private let parser: FileJSONParserProtocol
+    private let session: URLSession
+    
+    init(reader: FileReaderServiceProtocol,
+         parser: FileJSONParserProtocol,
+         session: URLSession = URLSession.shared) {
+        self.parser = parser
+        self.reader = reader
+        self.session = session
+    }
+    
+    func download<T: Decodable>(from fileURL: String, responseType: T.Type, handler: @escaping (Result<[T]>) -> Void) {
+        guard let url = URL(string: fileURL) else {
+            handler(.error(DownloadError.invalidURL))
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        let dataTask = self.session.downloadTask(with: request) { (localURL, _, error) in
+            guard error == nil else {
+                handler(.error(error))
+                return
+            }
+            
+            guard let localURL = localURL else {
+                handler(.error(DownloadError.unableToDownload))
+                return
+            }
+            
+            do {
+                let file = try self.reader.readFile(from: localURL)
+                
+                guard let result: [T] = self.parser.parse(lines: file) else {
+                    handler(.error(ParseError.unableToSerialize))
+                    return
+                }
+                
+                // TODO: Should check empty line?
+                
+                handler(.success(result))
+            } catch {
+                handler(.error(error))
+            }
+        }
+        dataTask.resume()
+    }
+}
---
+++ IntercomChallengeFramework/IntercomChallengeFramework/Core/Result.swift
@@ -0,0 +1,14 @@
+//
+//  Result.swift
+//  IntercomChallengeFramework
+//
+//  Created by Ohanna Perre on 10/01/2019.
+//  Copyright © 2019 Ohanna Perre. All rights reserved.
+//
+
+import Foundation
+
+enum Result<T> {
+    case success(T?)
+    case error(Error?)
+}
