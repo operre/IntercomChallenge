10dc9882e819cda861141b25eb5554215505b949
Ohanna Perre <ohanna.perre@gmail.com>
2019-01-10 12:59:32 +0000

    Create an enum of parse error + update gitignore

---
+++ IntercomChallengeFramework/IntercomChallengeFramework/Core/FileParser/ParseError.swift
@@ -0,0 +1,14 @@
+//
+//  ParseError.swift
+//  IntercomChallengeFramework
+//
+//  Created by Ohanna Perre on 10/01/2019.
+//  Copyright © 2019 Ohanna Perre. All rights reserved.
+//
+
+import Foundation
+
+enum ParseError: Error {
+    case unableToSerialize
+    case emptyFile
+}
